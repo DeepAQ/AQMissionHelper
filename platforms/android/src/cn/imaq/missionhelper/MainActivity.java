84cc7a74adc11130ea60d22f3f3d87d05b82929a
Monkey Liang <i@aq741.com>
2016-10-12 12:31:14 +0800

    press back twice to exit on Android

---
+++ platforms/android/src/cn/imaq/missionhelper/MainActivity.java
@@ -0,0 +1,66 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+ */
+
+package cn.imaq.missionhelper;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
+import org.apache.cordova.*;
+
+public class MainActivity extends CordovaActivity
+{
+    private AMapLocationClient locationClient = null;
+    private long backTime = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // Set by <content src="index.html" /> in config.xml
+        loadUrl(launchUrl);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (locationClient != null) {
+            locationClient.stopLocation();
+            locationClient.onDestroy();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        long time = System.currentTimeMillis();
+        if (time - backTime > 2000) {
+            Toast.makeText(getApplicationContext(), "Press again to exit", Toast.LENGTH_SHORT).show();
+            backTime = time;
+        } else {
+            System.exit(0);
+        }
+    }
+}
